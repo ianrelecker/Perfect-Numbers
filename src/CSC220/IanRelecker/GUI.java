824019d30706ce9db199a4b22cf3ba3097745e41
ianrelecker <ianrelecker@gmail.com>
2020-11-04 08:29:17 -0800

    wednesday morning

--- src/CSC220/IanRelecker/GUI.java
+++ src/CSC220/IanRelecker/GUI.java
@@ -5,12 +5,15 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+// What should i do to get the results out of the thread
+// how do I make sure everything updates
+
 public class GUI extends JFrame {
 
     public static final long serialVersionUID = 1L;
 
-    private final int width = 600;
-    private final int height = 500;
+    private final int width = 500;
+    private final int height = 600;
     private int amountToRun;
     private MainPanel mainPanel;
 
@@ -19,7 +22,7 @@ public class GUI extends JFrame {
         setSize(width, height);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new BorderLayout(5,5));
+        setLayout(new BorderLayout(1,1));
         mainPanel = new MainPanel();
         this.add(mainPanel, BorderLayout.CENTER);
         setVisible(true);
@@ -34,18 +37,26 @@ public class GUI extends JFrame {
         private JTextField fieldStatus;
         private JTextField numberToRun;
         private JScrollPane scrollPaneOutput;
+        private JTextArea textArea;
 
-        private int numberint;
+        private int numberInt;
         private String statusThread = "";
+        private long[] results;
+        private findNums finder;
+        private Thread.State state;
+
 
         MainPanel(){
-            Thread a;
             perfectNumbers = new JButton("Perfect Numbers");
             perfectNumbers.addActionListener(
                     new ActionListener() {
                         @Override
                         public void actionPerformed(ActionEvent e) {
-                            findNums finder = new findNums(numberint);
+                            findNums finder = new findNums(numberInt);
+                            results = finder.getResults();
+                            for (int i=0; i<results.length; i++) {
+                                textArea.append(String.valueOf(results[i]));
+                            }
                         }
                     }
             );
@@ -54,22 +65,28 @@ public class GUI extends JFrame {
                     new ActionListener() {
                         @Override
                         public void actionPerformed(ActionEvent e) {
-
+                            state = finder.getState();
+//                            fieldStatus.setEditable(true);
+                            fieldStatus.setText(String.valueOf(state));
+//                            fieldStatus.setEditable(false);
                         }
                     }
             );
 
-            setLayout(new FlowLayout());
-            fieldStatus = new JTextField(statusThread, 8);
 
+            setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+            fieldStatus = new JTextField(statusThread, 6);
+            numberToRun = new JTextField(statusThread, 6);
+            textArea = new JTextArea(7,8);
+            scrollPaneOutput.add(textArea);
             this.add(perfectNumbers);
-//            this.add(buttonStatus);
-//            this.add(fieldStatus);
-//            this.add(numberToRun);
+            this.add(buttonStatus);
+            this.add(fieldStatus);
+            this.add(numberToRun);
 
-//            scrollPaneOutput.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-//            scrollPaneOutput.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-//            this.add(scrollPaneOutput);
+            scrollPaneOutput.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+            scrollPaneOutput.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+            this.add(scrollPaneOutput);
         }
     }
 
--- src/CSC220/IanRelecker/findNums.java
+++ src/CSC220/IanRelecker/findNums.java
@@ -1,10 +1,20 @@
 package CSC220.IanRelecker;
 
 public class findNums extends Thread{
+    private int amt;
+    public long[] results;
 
     public findNums(int amountToRun) {
+        this.amt = amountToRun;
+    }
         //may need to add long[] something to get the values out
-        Main.running(amountToRun);
+//        Main.running(amountToRun);
+    public void run(){
+        results = Main.running(amt);
+    }
+
+    public long[] getResults(){
+        return results;
     }
 
 }
